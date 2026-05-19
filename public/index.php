c218ca0079c94e0a936b5f89d4c5a4d8c4358679
pax <pax@mioritics.ro>
2026-05-19 14:22:16 +0300

    add viewport

--- public/index.php
+++ public/index.php
@@ -27,6 +27,7 @@ $tiers = [
 <html lang="ro">
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <title>Oțios — Cuvinte Uitate</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
